4251f3470d0c8a70400ca266de56614326351c5f
Erich Steiger <info@erichsteiger.com>
2024-05-18 08:55:15 +0200

    build and run sonarqube

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./mvnw install
+./mvnw clean verify install sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=mvn -Dsonar.password=mvn
+
+cd com.erichsteiger.eopt.bo
+../mvnw sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=mvn -Dsonar.password=mvn
+cd ../com.erichsteiger.eopt.dao
+../mvnw sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=mvn -Dsonar.password=mvn
+cd ../com.erichsteiger.eopt.app
+../mvnw sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=mvn -Dsonar.password=mvn
